cfcd2b7597b0f0c8c9fbc3fcf1e1ad41a12614f6
Tsutomu Ogasawara <info@ogaoga.org>
2010-10-12 22:16:13 +0900

    Added add button.

---
+++ Classes/FormViewController.h
@@ -0,0 +1,16 @@
+//
+//  FormViewController.h
+//  iPhoneSampleApplication
+//
+//  Created by ogaoga on 10/10/12.
+//  Copyright 2010 ogaoga.org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface FormViewController : UIViewController {
+
+}
+
+@end
